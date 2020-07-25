5a93fe1c1d702769620cea224dd1c7ba400e2ac3
Antonio Augusto Silva Oliveira Neto <augusto.soliveiran@gmail.com>
2020-07-25 16:55:40 -0300

    Initial commit

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  name = ''
+
+  constructor(private router:ActivatedRoute) { }
+
+  ngOnInit() {
+    //console.log(this.router.snapshot.params['name']);
+    this.name = this.router.snapshot.params['name'];
+  }
+
+}
