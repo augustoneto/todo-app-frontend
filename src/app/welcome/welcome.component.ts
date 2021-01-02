bb76e22a032268806996e155abfbba051e461a75
Augusto Neto <augusto.soliveiran@gmail.com>
2021-01-02 19:42:40 -0300

    welcome service

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { WelcomeDataService } from '../service/data/welcome-data.service';
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -8,13 +10,42 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WelcomeComponent implements OnInit {
 
+  welcomeMessageFromService:string
   name = ''
 
-  constructor(private router:ActivatedRoute) { }
+  constructor(
+    private router:ActivatedRoute,
+    private service:WelcomeDataService) { }
 
   ngOnInit() {
     //console.log(this.router.snapshot.params['name']);
     this.name = this.router.snapshot.params['name'];
+  }
+
+  getWelcomeMessage() {
+    
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+    
+  }
+
+  getWelcomeMessageWithParameter() {
+    
+    this.service.executeHelloWorldServiceWithPathVariable(this.name).subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+    
+  }
+
+  handleSuccessfulResponse(response){
+    this.welcomeMessageFromService = response.message;
+  }
+
+  handleErrorResponse(error) {
+    this.welcomeMessageFromService = error.error.message;
   }
 
 }
